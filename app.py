44e302889b5931e00d015959fc03a461840da6cd
kbrzezinski1 <karol.brzezinski3@gmail.com>
2022-11-09 12:02:05 +0100

    changed format of dates

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ inflation_data = None
 
 @app.route("/")
 def home():
-    return "<p>Use to get current or history of prices of edo bonds</p><p>/bond/edo(month)(year)/(day)/price or /bond/edo(month)(year)/(day)/history</p> <p>example: https://edobond-api.herokuapp.com/bond/edo1030/11/price</p>"
+    return "<p>Use to get current or history of prices of edo bonds</p><p>/bond/edo(month)(year)/(day)/price or /bond/edo(month)(year)/(day)/history</p> <p>example: /bond/edo1030/11/price</p>"
 
 @app.before_first_request
 def before_first_request():
@@ -63,7 +63,7 @@ def post(pid, day):
     if diff.days > 0:
         td = datetime.utcnow() - bond_date
         price = 100 + td.days*(percentage/365)
-        return jsonify({'current_price': round(price, 2)}), 200, {'Content-Type': 'application/json; charset=utf-8'}
+        return jsonify({'current_price': format(round(price, 2), '.2f')}), 200, {'Content-Type': 'application/json; charset=utf-8'}
 
     price = 100 + percentage
     while diff.days < 0:
@@ -75,7 +75,7 @@ def post(pid, day):
         else:
             price = price + td.days*((inflation + marge)/365)/100*price
         diff = diff + relativedelta(years=1) + datetime.utcnow() - datetime.utcnow()
-    return jsonify({'current_price': round(price, 2)}), 200, {'Content-Type': 'application/json; charset=utf-8'}
+    return jsonify({'current_price': format(round(price, 2), '.2f')}), 200, {'Content-Type': 'application/json; charset=utf-8'}
 
 @app.route('/bond/<pid>/<day>/history', methods=['GET'])      
 def posthis(pid, day):   
@@ -97,6 +97,8 @@ def posthis(pid, day):
     day = int(day) - 1
     bond_date = bond_date + relativedelta(days=day)
     data = {}
+    bond_name = {"bond" : pid}
+    bond_list = []
     diff = bond_date + relativedelta(years=1) - datetime.utcnow()
     i = 0
     if diff.days > 0:
@@ -104,14 +106,23 @@ def posthis(pid, day):
         while i < td.days:                  
             price = 100 + i*(percentage/365)
             op_date = bond_date + relativedelta(days = i)
-            data[op_date.strftime("%d-%m-%Y")] = round(price, 2)
+            element = {
+                "date" : op_date.strftime("%Y-%m-%d"),
+                "price" : format(round(tmpprice, 2), '.2f')
+            }
+            bond_list.append(element)
             i += 1
-        return json.dumps(data, indent=4), 200, {'Content-Type': 'application/json; charset=utf-8'}
+        cc = bond_name,"values", bond_list
+        return json.dumps(cc, indent=4), 200, {'Content-Type': 'application/json; charset=utf-8'}
 
     while i <= 365:                  
         price = 100 + i*(percentage/365)
         op_date = bond_date + relativedelta(days = i)
-        data[op_date.strftime("%d-%m-%Y")] = round(price, 2)
+        element = {
+                "date" : op_date.strftime("%Y-%m-%d"),
+                "price" : format(round(price, 2), '.2f')
+            }
+        bond_list.append(element)
         i += 1
 
     price = 100 + percentage
@@ -125,13 +136,14 @@ def posthis(pid, day):
             return jsonify({'error':  "something went wrong with calculating inflation"}), 404
         td = datetime.utcnow() - (bond_date + relativedelta(years=i))
         if td.days > 365:
-            data.update(make_history_price(price, i, 1, data, inflation, marge, bond_date, td))
+            bond_list.extend(make_history_price(price, i, 1, inflation, marge, bond_date, td))
             price = price + (inflation + marge)/100*price
         else:
-            data.update(make_history_price(price, i, 1, data, inflation, marge, bond_date, td))
+            bond_list.extend(make_history_price(price, i, 1, inflation, marge, bond_date, td))
             price = price + td.days*((inflation + marge)/365)/100*price
-        diff = diff + relativedelta(days=365)  
-    return json.dumps(data, indent=4), 200, {'Content-Type':'application/json; charset=utf-8'} 
+        diff = diff + relativedelta(days=365)
+    cc = bond_name,"values", bond_list
+    return json.dumps(cc, indent=4), 200, {'Content-Type':'application/json; charset=utf-8'} 
 
 
 def get_inflation(bond_date, i):
@@ -162,10 +174,15 @@ def get_bond_date(soup):
     temp_date = (div.getText()).split("Okres oprocentowania:")[1].split()[0]
     return (datetime.strptime(temp_date, '%d.%m.%Y'))
 
-def make_history_price(price, i, j, data, inflation, marge, bond_date, td):
-    while j <= td.days  and j < 365:                 
+def make_history_price(price, i, j, inflation, marge, bond_date, td):
+    tlist = []  
+    while j <= td.days  and j <= 365:               
         tmpprice = price + j*((inflation + marge)/365)/100*price
         op_date = bond_date + relativedelta(days = j) + relativedelta(years = i)
-        data[op_date.strftime("%d-%m-%Y")] = round(tmpprice, 2)
         j += 1
-    return data
+        element = {
+                "date" : op_date.strftime("%Y-%m-%d"),
+                "price" : format(round(tmpprice, 2), '.2f')
+            }
+        tlist.append(element)
+    return tlist
